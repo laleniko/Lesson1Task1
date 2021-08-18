144095168c7ae644980865a7b606a727d9b10b4b
Nikolay <laleniko@gmail.com>
2021-08-18 16:16:50 +0300

    Initial Commit

---
+++ Lesson1Task1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson1Task1
+//
+//  Created by Николай Ларьков on 18.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
