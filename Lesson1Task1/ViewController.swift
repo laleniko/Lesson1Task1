ed3faa719da06e7b05ae43c701aed57e2b5b10c1
Nikolay <laleniko@gmail.com>
2021-08-18 18:17:34 +0300

    Constraints

--- Lesson1Task1/ViewController.swift
+++ Lesson1Task1/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+//    @IBOutlet weak var redColor: UIView!
+//    @IBOutlet weak var yellowColor: UIView!
+//    @IBOutlet weak var greenColor: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        redColor.layer.cornerRadius = 50
+//        yellowColor.layer.cornerRadius = 50
+//        greenColor.layer.cornerRadius = 50
+//        
+//        redColor.backgroundColor = .red
+//        yellowColor.backgroundColor = .yellow
+//        greenColor.backgroundColor = .green
     }
 
-
+//    @IBAction func pressButton(_ sender: Any) {
+//        print("azaza")
+//    }
+    
 }
 
