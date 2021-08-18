977d3a3f8fb28f38f1e43830750097e96a28e2d4
Nikolay <laleniko@gmail.com>
2021-08-18 21:59:33 +0300

    project logic

--- Lesson1Task1/ViewController.swift
+++ Lesson1Task1/ViewController.swift
@@ -8,27 +8,62 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-//    @IBOutlet weak var redColor: UIView!
-//    @IBOutlet weak var yellowColor: UIView!
-//    @IBOutlet weak var greenColor: UIView!
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-//        redColor.layer.cornerRadius = 50
-//        yellowColor.layer.cornerRadius = 50
-//        greenColor.layer.cornerRadius = 50
-//        
-//        redColor.backgroundColor = .red
-//        yellowColor.backgroundColor = .yellow
-//        greenColor.backgroundColor = .green
+    enum TrafficLights: String {
+        case redColor;
+        case yellowColor;
+        case greenColor;
+        case none;
     }
 
-//    @IBAction func pressButton(_ sender: Any) {
-//        print("azaza")
-//    }
+    @IBOutlet weak var redColor: UIView!
+    @IBOutlet weak var yellowColor: UIView!
+    @IBOutlet weak var greenColor: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
+    var workLight: TrafficLights = .none
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        redColor.layer.cornerRadius = 50
+        yellowColor.layer.cornerRadius = 50
+        greenColor.layer.cornerRadius = 50
+        startButton.layer.cornerRadius = 10
+
+        redColor.backgroundColor = .red
+        yellowColor.backgroundColor = .yellow
+        greenColor.backgroundColor = .green
+        
+        redColor.alpha = 0.3
+        yellowColor.alpha = 0.3
+        greenColor.alpha = 0.3
+    }
+
+
+    @IBAction func nextColor(_ sender: Any) {
+        if workLight != .none{
+            switch workLight {
+            case .redColor:
+                redColor.alpha = 0.3
+                yellowColor.alpha = 1
+                workLight = .yellowColor
+            case .yellowColor:
+                yellowColor.alpha = 0.3
+                greenColor.alpha = 1
+                workLight = .greenColor
+            case .greenColor:
+                redColor.alpha = 1
+                greenColor.alpha = 0.3
+                workLight = .redColor
+            default:
+               break
+            }
+            
+        } else {
+            startButton.setTitle("NEXT", for: .normal)
+            redColor.alpha = 1
+            workLight = .redColor
+        }
+    }
     
 }
 
